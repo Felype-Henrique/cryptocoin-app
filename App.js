57de6102905ac482a30021c25a263ded09c7907b
Felype Henrique <felypehenrique25@gmail.com>
2022-02-08 11:47:22 -0300

    add sample data

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ListItem from './components/ListItem';
 
+import { SAMPLE_DATA} from './assets/data/sampleData'
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -11,7 +13,13 @@ export default function App() {
       </View>
       <View style={styles.divider}/>
 
-      <ListItem />
+      <ListItem  
+      name={SAMPLE_DATA[0].name} 
+      symbol={SAMPLE_DATA[0].symbol}
+      currentPrice={SAMPLE_DATA[0].current_price}
+      priceChangePercentage7d={SAMPLE_DATA[0].price_change_percentage_7d_in_currency}
+      logoUrl={SAMPLE_DATA[0].image}
+      />
     </View>
   );
 }
